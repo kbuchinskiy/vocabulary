731112a7f305ba9c970480a410d6f1914b377fa5
KBuchynskyi <kyrylo.buchynskyi@onseo.biz>
2021-04-01 23:34:08 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>vocabulary</h1>
+    <br />
+    <input type="text" v-model="wordInput" />
+    <button v-show="wordsToDisplay.length === 0" @click="addWord">add</button>
+    <div class="words-list">
+      <div class="word" v-for="(word, index) in wordsToDisplay" :key="index">
+        {{ word.en }}
+        <span class="separator"> — </span>
+        {{ word.ru }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import _ from 'lodash'
+import wordService from '@/api/wordService'
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      wordInput: '',
+      words: ['lorem', 'durem'],
+      wordsToDisplay: [],
+    }
   },
-};
+  methods: {
+    onWordChange: _.throttle(function (current) {
+      this.wordsToDisplay = this.words.filter((w) => w.en.startsWith(current))
+    }, 500),
+    addWord() {
+      this.words.push(this.wordInput)
+      this.wordInput = ''
+    },
+    async getWords() {
+      const { data } = await wordService.getWords()
+      this.words = data
+      this.wordsToDisplay = this.words
+    },
+  },
+  watch: {
+    wordInput(current) {
+      this.onWordChange(current)
+    },
+  },
+  async created() {
+    await this.getWords()
+  },
+}
 </script>
 
 <style lang="scss">
@@ -24,5 +60,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.words-list {
+  margin: 3% 20% 0;
+}
+
+.word {
+  display: flex;
+  margin-bottom: 0.5rem;
+
+  padding: 0.3rem;
+  border-bottom: 1px solid rgb(207, 207, 207);
+
+  .separator {
+    display: inline-block;
+    margin: 0 1rem;
+  }
 }
 </style>
