69b78618e205fee14591557ea77c4181b82249d2
kyrylo.buchynskyi <kyrylo.buchynskyi@danads.se>
2022-05-04 15:18:06 +0300

    CORS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import router from './routes';
 import connectDB from './config/db';
 import colors from 'colors';
 import errorHandler from './middleware/error';
+import cors from 'cors';
 
 dotenv.config({ path: './config/config.env' });
 
@@ -13,7 +14,9 @@ connectDB();
 const PORT = process.env.PORT;
 const app = express();
 
+// order is important
 app.use(morgan('tiny'));
+app.use(cors());
 app.use(express.json());
 app.use('/api/words', router);
 app.use(errorHandler);
