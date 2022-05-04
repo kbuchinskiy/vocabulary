63bd76187974548411077a45266b75bee84d866d
kyrylo.buchynskyi <kyrylo.buchynskyi@danads.se>
2022-05-04 15:17:07 +0300

    Word model updated

--- models/Word.js
+++ models/Word.js
@@ -4,9 +4,15 @@ const WordSchema = new mongoose.Schema({
   origin: {
     type: String,
     required: [true, 'Provide word, please'],
+    index: true,
     unique: true,
     trim: true,
     maxlength: [50, 'Word can`t be more than 50 characters']
+  },
+  phonetic: {
+    type: String,
+    trim: true,
+    maxlength: [50, 'Phonetic can`t be more than 50 characters']
   },
   translation: {
     type: String,
@@ -19,11 +25,12 @@ const WordSchema = new mongoose.Schema({
     trim: true,
     maxlength: [300, 'Example can`t be more than 200 characters']
   },
-  definition: {
-    type: String,
-    trim: true,
-    maxlength: [300, 'Definition can`t be more than 200 characters']
+  definitions: {
+    type: Array,
   },
+  imgUrl: {
+    type: String,
+  }
 });
 
 export default mongoose.model('Word', WordSchema);
